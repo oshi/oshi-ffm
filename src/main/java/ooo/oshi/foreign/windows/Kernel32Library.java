5e70930560e056c0ec8f10058a7dbd7ca24a4273
Ronan D'Souza <ronan696@gmail.com>
2022-10-26 18:15:09 +0530

    feat: port more Kernel32 API calls
    - port GetTempPathW, CloseHandle, OpenProcess,
     QueryFullProcessImageNameW
    - handle exception meanfully with getLastError call
    - implement Wrapper for getLastError exception
    
    Signed-off-by: Ronan D'Souza <ronan696@gmail.com>

--- src/main/java/ooo/oshi/foreign/windows/Kernel32Library.java
+++ src/main/java/ooo/oshi/foreign/windows/Kernel32Library.java
@@ -11,6 +11,7 @@ import static java.lang.foreign.ValueLayout.ADDRESS;
 import java.lang.foreign.Addressable;
 import java.lang.foreign.FunctionDescriptor;
 import java.lang.foreign.Linker;
+import java.lang.foreign.MemoryAddress;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.SegmentAllocator;
 import java.lang.foreign.SymbolLookup;
@@ -18,6 +19,7 @@ import java.lang.foreign.ValueLayout;
 import java.lang.invoke.MethodHandle;
 
 import ooo.oshi.util.ParseUtil;
+import ooo.oshi.util.platform.windows.Win32Exception;
 
 public class Kernel32Library {
 
@@ -92,5 +94,130 @@ public class Kernel32Library {
     private static final MethodHandle getComputerName = methodHandle("GetComputerNameW",
             FunctionDescriptor.of(JAVA_BOOLEAN, ADDRESS, ADDRESS));
     
+	/**
+	 * Retrieves the path of the directory designated for temporary files.
+	 *
+	 * @return the temporary file path.
+	 */
+	public static String getTempPath() {
+		try {
+			SegmentAllocator allocator = SegmentAllocator.implicitAllocator();
+			// TODO: Handle case if Temp path length exceeds predefined buffer size
+			Addressable buffer = allocator.allocate(WinBase.MAX_PATH);
+			int nBufferLength = WinBase.MAX_PATH;
+			if ((int) getTempPath.invoke(nBufferLength, buffer) == 0) {
+				throw new Win32Exception(getLastError());
+			}
+			byte[] bytes = ((MemorySegment) buffer).toArray(ValueLayout.JAVA_BYTE);
+			return ParseUtil.parseByteArrayToUtf16(bytes);
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
+	}
 
+	private static final MethodHandle getTempPath = methodHandle("GetTempPathW",
+			FunctionDescriptor.of(JAVA_INT, JAVA_INT, ADDRESS));
+
+	/**
+	 * Closes an open object handle.
+	 *
+	 * @param h The handle to be closed
+	 */
+	public static void closeHandle(Addressable h) {
+		if (h == null) {
+			return;
+		}
+
+		try {
+			if (!(boolean) closeHandle.invokeExact(h)) {
+				throw new Win32Exception(getLastError());
+			}
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static final MethodHandle closeHandle = methodHandle("CloseHandle",
+			FunctionDescriptor.of(JAVA_BOOLEAN, ADDRESS));
+
+
+	/**
+	 * Opens an existing local process object.
+	 *
+	 * @param desiredAccess The access to the process object.
+	 * @param inheritHandle Processes created by this process should inherit the handle
+	 * @param processId The identifier of the local process to be opened.
+	 * @return the open handle to the specified process.
+	 */
+	public static Addressable openProcess(int desiredAccess, boolean inheritHandle, int processId) {
+		try {
+			Addressable hProcess = (MemoryAddress) openProcess.invokeExact(desiredAccess, inheritHandle, processId);
+			if (hProcess == null) {
+				throw new Win32Exception(getLastError());
+			}
+			return hProcess;
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static final MethodHandle openProcess = methodHandle("OpenProcess",
+			FunctionDescriptor.of(ADDRESS, JAVA_INT, JAVA_BOOLEAN, JAVA_INT));
+
+	private static final String queryFullProcessImageName(Addressable hProcess, int dwFlags) {
+		try {
+			SegmentAllocator allocator = SegmentAllocator.implicitAllocator();
+			int size = WinBase.MAX_PATH; // Start with MAX_PATH, then increment with 1024 each iteration
+			// TODO: Handle case if process image length exceeds predefined buffer size
+			Addressable lpExeName = allocator.allocate(size);
+			Addressable lpdwSize = allocator.allocate(JAVA_INT, size);
+			if (!(boolean) queryFullProcessImageName.invokeExact(hProcess, dwFlags, lpExeName, lpdwSize)) {
+				throw new Win32Exception(getLastError());
+			}
+			byte[] bytes = ((MemorySegment) lpExeName).toArray(ValueLayout.JAVA_BYTE);
+			return ParseUtil.parseByteArrayToUtf16(bytes);
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
+    }
+
+	private static final MethodHandle queryFullProcessImageName = methodHandle("QueryFullProcessImageNameW",
+            FunctionDescriptor.of(JAVA_BOOLEAN, ADDRESS, JAVA_INT, ADDRESS, ADDRESS));
+
+	/**
+	 * Retrieves the full name of the executable image for the specified process.
+	 *
+	 * @param pid The identifier of the local process to be opened.
+	 * @param dwFlags flags for path format of returned path.
+	 * @return the full name of the executable image for the specified process.
+	 */
+	public static final String queryFullProcessImageName(int pid, int dwFlags) {
+		Addressable hProcess = null;
+		Win32Exception we = null;
+
+		try {
+			hProcess = (Addressable) openProcess(WinBase.PROCESS_QUERY_INFORMATION | WinBase.PROCESS_VM_READ, false,
+					pid);
+			if (hProcess == null) {
+				throw new Win32Exception(getLastError());
+			}
+			return queryFullProcessImageName(hProcess, dwFlags);
+		} catch (Win32Exception e) {
+			we = e;
+			throw we; // re-throw to invoke finally block
+		} finally {
+			try {
+				closeHandle(hProcess);
+			} catch (Win32Exception e) {
+				if (we == null) {
+					we = e;
+				} else {
+					// Suppress Win32 error for closeHandle
+				}
+			}
+			if (we != null) {
+				throw we;
+			}
+		}
+	}
 }
--- src/main/java/ooo/oshi/foreign/windows/WinBase.java
+++ src/main/java/ooo/oshi/foreign/windows/WinBase.java
@@ -5,4 +5,14 @@ public class WinBase {
 	
 	public static final int MAX_COMPUTERNAME_LENGTH = 31;
 	
+	public static final int MAX_PATH = 260;
+	
+	public static final int PROCESS_QUERY_INFORMATION = 0x0400;
+	
+	public static final int PROCESS_VM_READ = 0x0010;
+	
+	public static final int ERROR_INSUFFICIENT_BUFFER = 122;
+	
+	public static final int PROCESS_NAME_NATIVE = 1;
+	
 }
